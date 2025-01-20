14c601c57e96a387b80f615bbfc8be85264c397e
Pedro <pedrodelmonico@hotmail.com>
2025-01-20 18:22:48 -0300

    feat: crete layout to the root app and store route

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+export const metadata: Metadata = {
+  title: 'dev store',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html className={`${inter.variable} antialiased`} lang="pt">
+      <body className="bg-zinc-950 text-zinc-50">{children}</body>
+    </html>
+  )
+}
