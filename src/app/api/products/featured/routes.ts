b2797d032625004456abd385efab334155e29f6f
Pedro <pedrodelmonico@hotmail.com>
2025-01-24 01:00:16 -0300

    feat: adding meta data for bettle SEO
    refactor(layout): using the created variable
    feat: adding the product type
    feat: adding a route for get product by slug
    feat: adding loading screen for home
    feat: adding a route for get products featured

--- src/app/api/products/featured/routes.ts
+++
@@ -1,7 +0,0 @@
-import data from '../../data.json'
-
-export async function GET() {
-  const featuredProducts = data.products.filter((item) => item.featured)
-
-  return Response.json(featuredProducts)
-}
---
+++ src/data/types/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number
+  title: string
+  slug: number
+  price: number
+  image: string
+  description: string
+}
